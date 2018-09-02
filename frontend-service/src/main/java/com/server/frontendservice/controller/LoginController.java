0b2d2878333ae2d291f8b4845ce1d47c1314e095
Chris Mepham <chris.mepham@cognitran.com>
2018-09-02 16:23:27 +0100

    Security fix attempt.

--- frontend-service/src/main/java/com/server/frontendservice/controller/LoginController.java
+++ frontend-service/src/main/java/com/server/frontendservice/controller/LoginController.java
@@ -9,11 +9,6 @@ import java.util.Map;
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public void login(Model model) {
-        model.addAttribute("showNavigation", false);
-    }
-
     @GetMapping("/logout")
     public void logout(Map<String, Object> model) {
         model.put("logout", Boolean.valueOf(true));

ca25914d8a684eeb6b4f3c8a75e24f5f7bfbef64
Chris Mepham <chris.mepham@cognitran.com>
2018-09-17 15:37:35 +0100

    Removed unused imports.

--- frontend-service/src/main/java/com/server/frontendservice/controller/EmailController.java
+++ frontend-service/src/main/java/com/server/frontendservice/controller/EmailController.java
@@ -1,17 +1,12 @@
 package com.server.frontendservice.controller;
 
 import com.server.common.model.Email;
-import com.server.common.model.Property;
 import com.server.frontendservice.service.EmailService;
-import com.server.frontendservice.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Arrays;
 import java.util.List;

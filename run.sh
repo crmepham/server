6dea66d5d0af788890809f62c1dfc0512de564af
Chris Mepham <chris.mepham@cognitran.com>
2019-03-30 16:04:40 +0000

    Updated run script.

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+echo 'Started backend...'
 java -jar backend-service/target/backend-service-1.0-SNAPSHOT.jar &
+
+sleep 3
+
+echo 'Started crawler...'
 java -jar crawler-service/target/crawler-service-1.0-SNAPSHOT.jar &
+
+sleep 3
+
+echo 'Started frontend...'
 java -jar frontend-service/target/frontend-service-1.0-SNAPSHOT.jar &
-echo 'Started server...'
+
+sleep 5
+echo 'Servers started.'
